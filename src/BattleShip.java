2c07232ab6b8f86da8097dbde5860e223d07b14c
jte91 <j3sh0n3dward5@gmail.com>
2022-11-17 17:05:15 -0600

    update

--- src/BattleShip.java
+++ src/BattleShip.java
@@ -11,7 +11,12 @@ public class BattleShip extends JFrame
   private JPanel gamePanel;
   private int gridSize = 10;
   private int cellSize = 50;
+  private int numShips=10;
   private int[][] gameGrid;
+  private boolean[][] uncoveredGrid;
+  private boolean[][] flaggedGrid;
+  private static final int NONE=0;
+  private static final int SHIP=-1;
   private JMenuItem hostItem;
   private JMenuItem joinItem;
   private JMenuItem quitItem;
@@ -33,6 +38,9 @@ public class BattleShip extends JFrame
   private boolean debug = false;
   private volatile boolean lost = false;
   private volatile boolean won = false;
+  private volatile boolean hit = false;
+  private volatile boolean miss = false;
+
   private boolean started = false;
   // private AudioInputStream ais;
   // private AudioFormat[] fmt;
@@ -97,9 +105,7 @@ public class BattleShip extends JFrame
 
     statusLabel=new JLabel(" ");
     statusPanel.add(statusLabel);
-    // resetButton = new JButton("Reset");
-    // resetButton.addActionListener(ah);
-    // buttonPanel.add(resetButton);
+
     createBoard();
     setDefaultCloseOperation(EXIT_ON_CLOSE);
     setResizable(false);
@@ -109,14 +115,48 @@ public class BattleShip extends JFrame
 
   public void createBoard() 
   {
-    // if (timer != null)
-    // timer.interrupt();
+    if (timer != null)
+    timer.interrupt();
     gameGrid = new int[gridSize][gridSize];
+    //uncoveredGrid=new boolean[gridSize][gridSize];
+    //flaggedGrid=new boolean[gridSize][gridSize];
     lost = false;
     won = false;
     started = false;
     statusLabel.setText("Right now, sea is empty");
     timerLabel.setText("Time:      0:00.0");
+    int x;
+    int y;
+    for(int i=0; i<numShips; i++)
+    {
+      do
+      {
+        x=(int)(Math.random() * (gridSize-1)+.5);
+        y=(int)(Math.random() * (gridSize-1)+.5);
+      }
+      while(gameGrid[x][y] != NONE);
+      gameGrid[x][y]=SHIP;
+    }
+    for(int i=0;i<gridSize;i++)
+    {
+      for(int j=0;j<gridSize;j++)
+      {
+        if(gameGrid[i][j]==SHIP) continue;
+        // gameGrid[i][j]=countMines(i,j);
+      }
+    }
+  }
+
+  private void checkWin()
+  {
+    won=true;
+    for(int i=0;i<gridSize;i++)
+    {
+      for(int j=0;j<gridSize;j++)
+      {
+        if(!uncoveredGrid[i][j]&&gameGrid[i][j]==SHIP) won=false;
+      }
+    }
   }
 
   private class GamePanel extends JPanel 
@@ -132,6 +172,45 @@ public class BattleShip extends JFrame
       for(int i=0;i<gridSize-1;i++)
       {
         g.drawLine(i*cellSize+cellSize,0,i*cellSize+cellSize,getHeight());
+      }
+      for(int i=0;i<gridSize;i++)
+      {
+        for(int j=0;j<gridSize;j++)
+        { 
+          if(uncoveredGrid[i][j])
+          {
+            g.setColor(Color.BLUE);
+            if(gameGrid[i][j]>0) 
+                 g.drawString(""+gameGrid[i][j],i*cellSize+7,j*cellSize+15);
+          }
+          else
+          {
+            g.setColor(Color.GRAY);
+            g.fillRect(i*cellSize,j*cellSize,20,20);
+          }
+          if(hit&&gameGrid[i][j]==SHIP)
+          {
+            if(uncoveredGrid[i][j]) g.setColor(Color.RED);
+            else g.setColor(Color.BLUE);
+            g.drawLine(i*cellSize+10,j*cellSize+8,i*cellSize+12,j*cellSize+4);
+            g.fillOval(i*cellSize+5,j*cellSize+6,11,11);
+            g.setColor(Color.WHITE);
+            g.drawLine(i*cellSize+8,j*cellSize+10,i*cellSize+10,j*cellSize+10);
+          }
+          if(flaggedGrid[i][j])
+          {
+            g.setColor(Color.BLACK);
+            g.drawLine(i*cellSize+5,j*cellSize+5,i*cellSize+5,j*cellSize+19);
+            g.setColor(Color.RED);
+            g.fillRect(i*cellSize+6,j*cellSize+6,10,6);
+          }
+          if(debug)
+          {
+            g.setColor(Color.LIGHT_GRAY);
+            if(!uncoveredGrid[i][j]) 
+                    g.drawString(""+gameGrid[i][j],i*cellSize+6,j*cellSize+15);
+          }
+        }
       }
     //   for (int j = 0; j < 20; j++) {
     //     for (int i = 0; i < 20; i++) {
@@ -155,22 +234,21 @@ public class BattleShip extends JFrame
   }
     public void deployPlayerShips() 
     {
-      Scanner input = new Scanner(System.in);
       statusLabel.setText("Deploy your ships:");
       // Deploying five ships for player
       BattleShip.playerShips = 5;
       for (int i = 1; i <= BattleShip.playerShips;) {
         statusLabel.setText("Enter X coordinate for your ship: ");
-        int x = input.nextInt();
+        //int x = input.nextInt();
         statusLabel.setText("Enter Y  coordinate for your ship: ");
-        int y = input.nextInt();
+        //int y = input.nextInt();
 
-        if ((x >= 0 && x < numRows) && (y >= 0 && y < numCols) && (grid[x][y] == " ")) {
-          grid[x][y] = "@";
+        //if ((x >= 0 && x < numRows) && (y >= 0 && y < numCols) && (grid[x][y] == " ")) {
+          //grid[x][y] = "@";
           i++;
-        } else if ((x >= 0 && x < numRows) && (y >= 0 && y < numCols) && grid[x][y] == "@")
+       // } else if ((x >= 0 && x < numRows) && (y >= 0 && y < numCols) && grid[x][y] == "@")
           System.out.println("You can't place two or more ships on the same location");
-        else if ((x < 0 || x >= numRows) || (y < 0 || y >= numCols))
+        //else if ((x < 0 || x >= numRows) || (y < 0 || y >= numCols))
           System.out.println("You can't place ships outside the " + numRows + " by " + numCols + " grid");
       }
       // printOceanMap();
@@ -180,8 +258,10 @@ public class BattleShip extends JFrame
     {
       public void actionPerformed(ActionEvent e) 
       {
-        createBoard();
-        gamePanel.repaint();
+        //createBoard();
+        // gamePanel.repaint();
+        deployPlayerShips();
+        statusLabel.setText("Deploy your ships:");
       }
     }
       //     if (out != null)
@@ -247,8 +327,18 @@ public class BattleShip extends JFrame
             timer=new TimerThread();
             timer.start();
           }
-        int i = e.getX() / 20;
-        int j = e.getY() / 20;
+        int i = e.getX() / cellSize;
+        int j = e.getY() / cellSize;
+        if(e.getButton()==MouseEvent.BUTTON1)
+        {
+          if(gameGrid[i][j]==SHIP) 
+          {
+            //uncoveredGrid[i][j]=true;
+            //timer.interrupt();
+            statusLabel.setText("You Hit a Ship!");
+            //new SoundThread(BOOM).start();
+          }
+        }
         model[i][j] = 1;
         out.println(i + "," + j);
         gamePanel.repaint();
